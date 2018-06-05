ec2587fd553c0b4a2a6b6eb09387634ee7408fb1
Raul Schmidlin <raul.schmidlin@schneider-electric.com>
2018-06-05 19:24:17 +0200

    Better chance of usage of e2wm by toggling using M-+

--- user-defaults/window-configuration.el
+++ user-defaults/window-configuration.el
@@ -8,10 +8,30 @@
   (golden-ratio-mode 1)
   (advice-add #'ace-window :after #'golden-ratio))
 
+(defvar e2wm:active
+  "Defines if e2wm has been activated"
+  nil)
+
+(defun e2wm:toggle ()
+  "Toggles if window management is done by e2wm"
+  (interactive)
+  (if e2wm:active
+      (progn
+        (e2wm:stop-management)
+        (global-linum-mode)
+        (golden-ratio-mode t)
+        (advice-add #'ace-window :after #'golden-ratio))
+    (progn
+      (global-linum-mode -1)
+      (golden-ratio-mode -1)
+      (advice-remove #'ace-window #'golden-ratio)
+      (e2wm:start-management)))
+  (setq e2wm:active (not e2wm:active)))
+
 (use-package e2wm
   :config
   (require 'e2wm)
-  (global-set-key (kbd "M-+") 'e2wm:start-management))
+  (global-set-key (kbd "M-+") 'e2wm:toggle))
 
 (set-face-attribute 'default nil :height 140)
 
